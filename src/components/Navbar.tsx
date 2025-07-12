825235b74ad126d97ec4806c563900282f18fe79
Bad-YX <cabyounes@gmail.com>
2025-07-12 12:44:47 +0100

    fixed the home reload problem when clicking the logo by deleting the search params after reload

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,14 +3,18 @@
 import Link from "next/link";
 import Image from "next/image";
 import CustomButton from "./CustomButton";
-
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
-
+    const handleReturnToHomePage = () => {
+        const router = useRouter();
+        router.replace("/");
+        window.location.reload();
+    }
     return (
         <header className="w-full absolute z-10">
             <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
-                <Link href="/" className="flex justify-center items-center" onClick={() => {location.reload()}}>
+                <Link href="/" className="flex justify-center items-center" onClick={handleReturnToHomePage}>
                     <Image src="/logo1.svg" alt="logo" width={118} height={18} className="object-contain" />
                 </Link>
                 <CustomButton 
