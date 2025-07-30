1e59c90d8e635497bd883a2d80997428fa788f6e
YX <cabyounes@gmail.com>
2025-07-30 18:12:06 +0100

    Update fetchdata.ts

--- src/utils/fetchdata.ts
+++ src/utils/fetchdata.ts
@@ -2,7 +2,7 @@
 import { CarProps, FilterProps } from '@/types';
 
 export async function fetchCars(filters: FilterProps){
-    const res = await fetch(process.env.URL + '/cars_data.json');
+    const res = await fetch(process.env.MYURL + '/cars_data.json');
     let data = await res.json();
     //loop through the filters if is there is any filter apply it on the result data
     for(const [key] of Object.entries(filters)){
